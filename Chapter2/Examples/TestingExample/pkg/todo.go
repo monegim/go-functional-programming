62c873bfb2deb4e4ee9e78d514c21dd21f9a68dc
Mostafa Negim <mostafa.negim@gmail.com>
2023-06-05 20:00:24 +0430

    Update: todo.go

--- Chapter2/Examples/TestingExample/pkg/todo.go
+++ Chapter2/Examples/TestingExample/pkg/todo.go
@@ -8,11 +8,28 @@ func main() {
 
 type Todo struct {
 	Text string
-	Db *Db
+	Db   *Db
 }
+
 func NewTodo() Todo {
 	return Todo{
 		Text: "",
-		Db: NewDB(),
+		Db:   NewDB(),
+	}
+}
+
+func (t *Todo) Write(s string) {
+	if t.Db.IsAuthorized() {
+		t.Text = s
+	} else {
+		panic("user not authorized to write")
+	}
+}
+
+func (t *Todo) Append(s string) {
+	if t.Db.IsAuthorized() {
+		t.Text += s
+	} else {
+		panic("user not authorized to append")
 	}
 }
